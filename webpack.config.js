9d155f0fde32d98c4943e4b756373d0200a0c4f3
Roman Marchenko <whisperdepth@gmail.com>
2020-08-07 20:10:05 +0300

    d

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const webpack = require("webpack");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === "production";
@@ -33,6 +34,9 @@ module.exports = (env, argv) => {
       new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({
         template: "./src/index.html",
+      }),
+      new CopyPlugin({
+        patterns: [{ from: "_redirects", to: "" }],
       }),
     ],
     resolve: {
