2f84fc117859d4355a9949c8a6dde962f715ddae
Roman Marchenko <whisperdepth@gmail.com>
2020-08-09 21:06:19 +0300

    a

--- src/board/flights.reducer.js
+++ src/board/flights.reducer.js
@@ -3,17 +3,16 @@ import { GET_FLIGHTS_LIST } from "./flights.actions";
 const initialValue = {
   departure: [],
   arrival: [],
-
 };
 
 export default (state = initialValue, action) => {
   switch (action.type) {
-    case  GET_FLIGHTS_LIST: 
-     return {
-       ...state,
+    case GET_FLIGHTS_LIST:
+      return {
+        ...state,
         departure: action.payload.flights.departure,
         arrival: action.payload.flights.arrival,
-     }
+      };
     default:
       return state;
   }
