4515ac684e32275083f8ba8d39e88e5c72009620
Roman Marchenko <whisperdepth@gmail.com>
2020-08-15 12:07:43 +0300

    refactoring

--- src/board/flights.selectors.js
+++ src/board/flights.selectors.js
@@ -1,12 +1,9 @@
-export const departureSelector = (state) => {
-  return state.flights.departure.filter(
+export const departureSelector = (state) =>
+  state.flights.departure.filter(
     (flight) => new Date(flight.time).getDate() === new Date().getDate()
   );
-};
 
-export const arrivalSelector = (state) => {
-  return state.flights.arrival.filter(
+export const arrivalSelector = (state) =>
+  state.flights.arrival.filter(
     (flight) => new Date(flight.time).getDate() === new Date().getDate()
   );
-};
-
