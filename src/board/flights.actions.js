f5424eb5676abae03a6868a78e5ee5ed80f196a4
Roman Marchenko <whisperdepth@gmail.com>
2020-08-15 12:43:09 +0300

    refactoring

--- src/board/flights.actions.js
+++ src/board/flights.actions.js
@@ -10,32 +10,28 @@ const getFlightsList = (flights) => ({
   },
 });
 
-export const setFlightsToStase = (filterText) => (dispatch) => {
+export const setFlightsToStase = (filterText) => (dispatch) =>
   fetchFlights()
     .then((flightsData) => {
-      const departure = flightsData.body.departure.map((flight) => {
-        return {
-          term: flight.term,
-          time: flight.timeDepExpectCalc,
-          city: flight["airportToID.city_en"],
-          status: flight.status,
-          airlineName: flight.airline.en.name,
-          airlineLogo: flight.airline.en.logoSmallName,
-          flightNum: flight["carrierID.code"] + flight.fltNo,
-        };
-      });
+      const departure = flightsData.body.departure.map((flight) => ({
+        term: flight.term,
+        time: flight.timeDepExpectCalc,
+        city: flight["airportToID.city_en"],
+        status: flight.status,
+        airlineName: flight.airline.en.name,
+        airlineLogo: flight.airline.en.logoSmallName,
+        flightNum: flight["carrierID.code"] + flight.fltNo,
+      }));
 
-      const arrival = flightsData.body.arrival.map((flight) => {
-        return {
-          term: flight.term,
-          time: flight.timeToStand,
-          city: flight["airportFromID.city_en"],
-          status: flight.status,
-          airlineName: flight.airline.en.name,
-          airlineLogo: flight.airline.en.logoSmallName,
-          flightNum: flight["carrierID.code"] + flight.fltNo,
-        };
-      });
+      const arrival = flightsData.body.arrival.map((flight) => ({
+        term: flight.term,
+        time: flight.timeToStand,
+        city: flight["airportFromID.city_en"],
+        status: flight.status,
+        airlineName: flight.airline.en.name,
+        airlineLogo: flight.airline.en.logoSmallName,
+        flightNum: flight["carrierID.code"] + flight.fltNo,
+      }));
 
       const filteredDeparture = filterFlightsSelector(departure, filterText);
       const filteredArrival = filterFlightsSelector(arrival, filterText);
@@ -48,4 +44,3 @@ export const setFlightsToStase = (filterText) => (dispatch) => {
       dispatch(getFlightsList(flights));
     })
     .catch(() => alert("Internal server error. Please, try again later."));
-};
