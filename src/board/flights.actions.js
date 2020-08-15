f046d49232979ea15ea1b9fab05022b74b968a8b
Roman Marchenko <whisperdepth@gmail.com>
2020-08-15 12:12:31 +0300

    refactoring

--- src/board/flights.actions.js
+++ src/board/flights.actions.js
@@ -3,53 +3,49 @@ import { fetchFlights } from "./flights.gateway";
 
 export const GET_FLIGHTS_LIST = "FLIGHTS/GET_FLIGHTS_LIST";
 
-const getFlightsList = (flights) => {
-  return {
-    type: GET_FLIGHTS_LIST,
-    payload: {
-      flights,
-    },
-  };
-};
+const getFlightsList = (flights) => ({
+  type: GET_FLIGHTS_LIST,
+  payload: {
+    flights,
+  },
+});
 
-export const setFlightsToStase = (filterText) => {
-  return (dispatch) => {
-    fetchFlights()
-      .then((flightsData) => {
-        const departure = flightsData.body.departure.map((flight) => {
-          return {
-            term: flight.term,
-            time: flight.timeDepExpectCalc,
-            city: flight["airportToID.city_en"],
-            status: flight.status,
-            airlineName: flight.airline.en.name,
-            airlineLogo: flight.airline.en.logoSmallName,
-            flightNum: flight["carrierID.code"] + flight.fltNo,
-          };
-        });
-
-        const arrival = flightsData.body.arrival.map((flight) => {
-          return {
-            term: flight.term,
-            time: flight.timeToStand,
-            city: flight["airportFromID.city_en"],
-            status: flight.status,
-            airlineName: flight.airline.en.name,
-            airlineLogo: flight.airline.en.logoSmallName,
-            flightNum: flight["carrierID.code"] + flight.fltNo,
-          };
-        });
-
-        const filteredDeparture = filterFlightsSelector(departure, filterText);
-        const filteredArrival = filterFlightsSelector(arrival, filterText);
-
-        const flights = {
-          departure: filteredDeparture,
-          arrival: filteredArrival,
+export const setFlightsToStase = (filterText) => (dispatch) => {
+  fetchFlights()
+    .then((flightsData) => {
+      const departure = flightsData.body.departure.map((flight) => {
+        return {
+          term: flight.term,
+          time: flight.timeDepExpectCalc,
+          city: flight["airportToID.city_en"],
+          status: flight.status,
+          airlineName: flight.airline.en.name,
+          airlineLogo: flight.airline.en.logoSmallName,
+          flightNum: flight["carrierID.code"] + flight.fltNo,
         };
+      });
 
-        dispatch(getFlightsList(flights));
-      })
-      .catch(() => alert("Internal server error. Please, try again later."));
-  };
+      const arrival = flightsData.body.arrival.map((flight) => {
+        return {
+          term: flight.term,
+          time: flight.timeToStand,
+          city: flight["airportFromID.city_en"],
+          status: flight.status,
+          airlineName: flight.airline.en.name,
+          airlineLogo: flight.airline.en.logoSmallName,
+          flightNum: flight["carrierID.code"] + flight.fltNo,
+        };
+      });
+
+      const filteredDeparture = filterFlightsSelector(departure, filterText);
+      const filteredArrival = filterFlightsSelector(arrival, filterText);
+
+      const flights = {
+        departure: filteredDeparture,
+        arrival: filteredArrival,
+      };
+
+      dispatch(getFlightsList(flights));
+    })
+    .catch(() => alert("Internal server error. Please, try again later."));
 };
