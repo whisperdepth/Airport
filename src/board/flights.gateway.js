64f74e4e18973a61a46fda9b6dc844459dec523d
Roman Marchenko <whisperdepth@gmail.com>
2020-08-22 12:51:44 +0300

    proxy server url added

--- src/board/flights.gateway.js
+++ src/board/flights.gateway.js
@@ -5,10 +5,9 @@ const proxyUrl = "https://cors-anywhere.herokuapp.com";
 const baseUrl = "https://api.iev.aero/api/flights";
 
 export const fetchFlights = () =>
-  fetch(`${proxyUrl}/${baseUrl}/${moment(new Date()).format("DD-MM-YYYY")}`).then(
-    (response) => {
-      if (response.ok) return response.json();
-      throw new Error();
-    }
-  );
-
+  fetch(
+    `${proxyUrl}/${baseUrl}/${moment(new Date()).format("DD-MM-YYYY")}`
+  ).then((response) => {
+    if (response.ok) return response.json();
+    throw new Error();
+  });
